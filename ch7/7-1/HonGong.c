846e230ad3002f76ef439d2f15bdc952a47218b0
MeteorLee <meteorproject529@gmail.com>
2023-07-16 01:19:38 +0900

    study: ch7 함수

---
+++ ch7/7-1/HonGong.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int sum(int x, int y);
+
+int main(void)
+{
+	int a = 10, b = 20;
+	int result;
+
+	result = sum(a, b);
+	printf("result : %d\n", result);
+
+	result = substract(a, b);
+	printf("result : %d\n", result);
+
+	return 0;
+
+}
+
+int sum(int x, int y)
+{
+	int temp;
+
+	temp = x + y;
+
+	return temp;
+
+}
+
+int substract(int x, int y)
+{
+	return x - y;
+}
